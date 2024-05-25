42f692c3d776dfee0620838bc2300f363e56ac7d
Gaël <prigentgael@gmail.com>
2024-05-25 10:18:44 +0200

    Added basics for git

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+
 router.get('/', (req, res) => {
 res.render('home');
 })
+router.get('/git', (req, res) => {
+    res.render('git');
+    })
+
 module.exports = router;
